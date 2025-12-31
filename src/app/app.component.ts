b22b55d40366e7aee89e11a759dadb0a85ecfd8f
Sarthak-Dhavale <sarthak.dhavale2003@gmail.com>
2025-12-31 23:44:20 +0530

    Implemented signal with search bar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, computed, Signal, WritableSignal, effect, signal} from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { SearchComponentComponent } from './search-component/search-component.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,SearchComponentComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'SignalDemo';
+  constructor(){
+    effect( () => {
+      console.log(`Count value is: ${this.count()}`)
+      console.log(`Double Count value is: ${this.doubleCount()}`)
+    })
+  }
+
+  // count = signal(10);
+  count : WritableSignal<number> = signal(10);
+  doubleCount : Signal<number> = computed( () => this.count() * 2);
+
+  updateCount(){
+    this.count.update( (value) => value  + 1);
+  }
+
+ 
+  // increment(){
+  //   this.count.set(this.count() + 1)
+  // }
+
+  // decrement(){
+  //   this.count.set(this.count() -1 )
+  // }
+
+  // updateValue(){
+  //   this.count.update( value => value + 1);
+  // }
 }
---
+++ src/app/search-component/search-component.component.ts
@@ -0,0 +1,33 @@
+import { CommonModule } from '@angular/common';
+import { Component, computed, OnInit, signal } from '@angular/core';
+import { ReactiveFormsModule,FormControl, FormGroup } from '@angular/forms';
+
+@Component({
+  selector: 'app-search-component',
+  standalone: true,
+  imports: [CommonModule, ReactiveFormsModule],
+  templateUrl: './search-component.component.html',
+  styleUrl: './search-component.component.css'
+})
+export class SearchComponentComponent implements OnInit {
+
+  cityList = signal<string[]>(["New York", "Los Angeles", "Chicago", "Houston", "Phoenix", "Philadelphia", "San Antonio", "San Diego", "Dallas", "San Jose"]);
+  searchTerm =  signal('')
+  filteredCities = computed(() => {
+    const search = this.searchTerm().toLowerCase();
+    return this.cityList().filter(city => city.toLowerCase().includes(search));
+
+  })
+
+  searchForm = new FormGroup({
+    search: new FormControl('')
+  })
+
+  ngOnInit(){
+
+    this.searchForm.get('search')?.valueChanges.subscribe(value => {
+      this.searchTerm.set(value ?? '');
+    })
+  }
+
+}
